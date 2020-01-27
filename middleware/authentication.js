4c17c4b1043ea84d8fe8158aebfc2489e953e9ba
cishocksr <cishockleysr@gmail.com>
2020-01-26 23:49:06 -0500

    middleware complete

---
+++ middleware/authentication.js
@@ -0,0 +1,38 @@
+const projects = require('../data/helpers/projectModel');
+const actions = require('../data/helpers/actionModel');
+
+function validateProjectID(req, res, next) {
+  const { id } = req.params;
+  projects
+    .get(id)
+    .then(data => {
+      if (data) {
+        req.data = data;
+        next();
+      } else {
+        res.status(404).json({
+          error: 'ID does not exist'
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+}
+
+function validateActionsID(req, res, next) {
+  const { id } = req.params;
+  actions.get(id).then(data => {
+    if (data) {
+      req.data = data;
+      next();
+    } else {
+      res.staus(404).json({
+        error: 'ID does not exist'
+      });
+    }
+  });
+}
+
+module.exports = { validateActionsID, validateProjectID };
