1923de56c50ec5e20817a3c0fd64dc6737a6224c
cishocksr <cishockleysr@gmail.com>
2020-01-26 23:04:45 -0500

    project router complete

--- routers/project-router.js
+++ routers/project-router.js
@@ -8,8 +8,61 @@ const router = express.Router();
 router.use(express.json());
 
 router.get('/', (req, res) => {
-  const messageOfTheDay = process.env.MOTD;
-  res.status(200).json({ motd: messageOfTheDay });
+  project
+    .get('/')
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.status(500).json({
+        error: 'No projects exist'
+      })
+    );
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  project
+    .get(id)
+    .then(data => res.status(200).json({ data }))
+    .catch(err =>
+      res.status(500).json({
+        error: 'No project matches id'
+      })
+    );
+});
+
+router.post('/', (req, res) => {
+  project
+    .insert(req.body)
+    .then(data => res.status(201).json(data))
+    .catch(err =>
+      res.status(500).json({
+        error: 'Could not add project'
+      })
+    );
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  project
+    .update(id, body)
+    .then(data => res.status(201).json(data))
+    .catch(err =>
+      res.status(500).json({
+        error: 'Could not update project'
+      })
+    );
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  project
+    .remove(id)
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.send(204).json({
+        error: 'Project could not be deleted'
+      })
+    );
+});
 module.exports = router;
